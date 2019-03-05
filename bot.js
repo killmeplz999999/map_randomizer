8fb6095145e3d062a837ca47ad4ec7ef7e6d85fc
killmeplz999999 <47834611+killmeplz999999@users.noreply.github.com>
2019-03-05 14:11:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,21 +12,16 @@ client.on('message', message => {
 
 
 			var map ='none ';
-			var rdmmap = Math.floor(Math.random() * 8);
+			var rdmmap = Math.floor(Math.random() * 3);
 			var bscmsg = "You will be playing the map ";
 			var pnmb = Math.floor(Math.random() * 2);
 			var pnmbf = 'text';
 			if (pnmb==0){pnmbf = 'as P1.';}
 			if (pnmb==1){pnmbf = 'as P2.';}
-if (rdmmap==0){map='Arc de Historique (HDNB54D6) ';}
-if (rdmmap==1){map='Clam Bay (5NABDY96) ';}
-if (rdmmap==2){map='Clearing (WCDH8GHH) ';}
-if (rdmmap==3){map='Forgotten Waterways (2GJVHY2Q) ';}
-if (rdmmap==4){map='Groovy Shadows... (8TUDU52Z) ';}
-if (rdmmap==5){map="Luna's Winter (FQHHR4WX)";}
-if (rdmmap==6){map='Skirmish (SJVEW3RQ) ';}
-if (rdmmap==7){map='A Thorn In Our Side (BPLF95JN) ';}
-if (rdmmap==8){map="Valder's Bane (7262BHZS)";}
+if (rdmmap==0){map='Dueling Peaks (Z27TXBWT) ';}
+if (rdmmap==1){map='Watch Your Back (V3Z6ELNU) ';}
+if (rdmmap==2){map='Hinterland Hijinks 1.3 (G8G5FURD) ';}
+if (rdmmap==3){map='Skirmish (SJVEW3RQ) ';}
     	
 		message.reply(bscmsg + map + pnmbf);
 
